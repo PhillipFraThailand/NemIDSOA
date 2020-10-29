b6d660817926155e2e32698527a05080aae1e24c
Phillip Eismark <phillipeismark@Phillips-MacBook-Pro.local>
2020-10-29 12:05:10 +0100

    CRUD for Gender and User

--- NemID_UserGenerator/NemIDUserGenerator.js
+++ NemID_UserGenerator/NemIDUserGenerator.js
@@ -8,10 +8,10 @@ var express = require('express')
 //var db = new sqlite3.Database('NemIDUsers.sqlite')
 var db = new sqlite3.Database('/Users/phillipeismark/Documents/SystemIntegration/si_mandatory_assignment_1/NemID_UserGenerator/NemIDUsers.sqlite')
 
-// 8088 is specified by the assignment
+// port
 const PORT = 8088
 
-// call the express function which returns an express application
+// instantiate express
 var app = express()
 
 // setup express to use json
@@ -28,7 +28,8 @@ app.get('/test', (req, res) => {
     console.log("NEW REQUEST")
     res.status(200).send({message: "Server is up and running... "});
 });
-// create response of nemID and statuscode
+
+// create nemId
 app.post('/generate-nemId', (req,res) => {  
     console.log("POST on /generate-nemId");
     body = JSON.stringify(req.body);
@@ -44,8 +45,7 @@ app.post('/generate-nemId', (req,res) => {
     res.status(201).send(response);
 });
 
-// create, update, delete, list all, list one by id (gender)
-// crud for users and gender change code to created and not 200 which is just ok
+// create user
 app.post('/create-user', (req, res) => {
     console.log(req);
     let data = req.body;
@@ -58,20 +58,20 @@ app.post('/create-user', (req, res) => {
     return res.status(201).send({"Response":"OK Creating..."})
 });
 
-//expects column to update, value to set column to, identifier, identifier value
-// CRUD for User
+// update user
+// expects column to update, value to set column to, identifier, identifier value
 app.patch('/update-user', (req, res) => {
     let data = req.body;
     let query = `UPDATE User SET ${data.Column} = "${data.Value}" WHERE ${data.Identifier}="${data.IdentifierValue}"`;
     db.run(query, (err) => {
         if (err){
             console.log(err)
-            console.log(query)
         }
     });
     return res.status(202).send({'Response': 'OK Updating...'})
 });
 
+// delete user
 app.delete('/delete-user', (req,res) => {
     let data = req.body;
     let query = `DELETE FROM User WHERE ${data.Identifier} = ${data.IdentifierValue}`
@@ -79,10 +79,11 @@ app.delete('/delete-user', (req,res) => {
         if (err) {
             console.log(err);
         }
-        return res.status(202).send({"Response": "OK Deleting..."})
+        return res.status(204).send({"Response": "OK Deleting..."})
     });
 });
 
+// get specific user
 app.get('/get-user', (req,res) => {
     let data = req.body;
     let query = `SELECT * FROM User WHERE ${data.Identifier}=${data.IdentifierValue}`;
@@ -95,8 +96,8 @@ app.get('/get-user', (req,res) => {
     });
 });
 
-app.get('/get-users', async (req, res) => {
-    let data = req.body;
+// get all users
+app.get('/get-users', (req, res) => {
     let query = "SELECT * FROM User";
     console.log(data);
     db.all(query, (err, rows) => {
@@ -109,10 +110,67 @@ app.get('/get-users', async (req, res) => {
     });
 });
 
-//next task is to create a gender label with id's 
-//  1 for male, 2 female, 3 binary
-// then we need to update all users to have a 1 or 2 or 3 for genderId
-// CRUD for Gender
+// CRUD for Gender here
+// create a genderlabel
+app.post('/create-genderLabel', (req, res) => {
+    let data = req.body; 
+    let query = `INSERT INTO Gender(Label) VALUES("${data.Label}")`;
+    db.run(query, (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+    return res.status(204).send({"Response":"OK Creating..."});
+});
+
+// delete a gender label
+app.delete('/delete-genderLabel', (req, res) => {
+    let data = req.body;
+    let query = `DELETE FROM Gender WHERE ${data.Identifier} = ${data.IdentifierValue}`;
+    db.run(query, (err) => {
+        if (err) {
+            console.log(err);
+        }
+        return res.status(204).send({"Response": "OK Deleting..."});
+    });
+});
+
+// get all gender labels
+app.get('/get-genderLabels', (req, res) => {
+    let query = "SELECT * FROM Gender";
+    db.all(query, (err, rows) => {
+        if (err) {
+            console.log(err);
+        } else {
+            return res.status(200).send(rows)
+        }
+    });
+});
+
+// get specific gender label
+app.get('/get-genderLabel', (req,res) => {
+    let data = req.body;
+    let query = `SELECT * FROM Gender WHERE ${data.Identifier}=${data.IdentifierValue}`;
+    db.each(query, (err, row) => {
+        if (err) {
+            console.log(err);
+        } else {
+            return res.status(200).send(row)
+        }
+    });
+});
+
+// update a gender label
+app.patch('/update-genderLabel', (req, res) => {
+    let data = req.body;
+    let query = `UPDATE Gender SET ${data.Column} = "${data.Value}" WHERE ${data.Identifier}="${data.IdentifierValue}"`;
+    db.run(query, (err) => {
+        if (err){
+            console.log(err)
+        }
+    });
+    return res.status(202).send({'Response': 'OK Updating...'})
+});
 
 // start the server
 app.listen(PORT, (err) => {
@@ -120,7 +178,7 @@ app.listen(PORT, (err) => {
         console.log(err)
         return;
     }
-    console.log('Listening on port: ', PORT )
+    console.log('Listening on port: ', PORT );
 });
 
 // generate random numbers and round it off
